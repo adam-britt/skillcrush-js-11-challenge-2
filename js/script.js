8637b7217b4ad1051009d9603f1fdf1c8311be4b
Adam Britt <adamrussellbritt@gmail.com>
2023-08-10 16:18:47 -0500

    final commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
-const vehicle = function(type, numWheels, color) {
+const button = document.querySelector("button");
+const repairList = document.querySelector(".need-repair");
+
+const createVehicle = function(type, numWheels, color) {
     const vehicle = {};  
     vehicle.type = type;
     vehicle.numWheels = numWheels;
     vehicle.color = color;
     
     return vehicle;
-}
+};
 
-const car = vehicle("car", 4, "blue");
-const bike = vehicle("bike", 2, "silver");
-const skateboard = vehicle("skateboard", 4, "black");
+const car = createVehicle("car", 4, "blue");
+const bike = createVehicle("bike", 2, "silver");
+const skateboard = createVehicle("skateboard", 4, "black");
 
 const myVehicles = [car, bike, skateboard];
 
@@ -17,4 +20,23 @@ for (let vehicle of myVehicles) {
     for (let key in vehicle) {
         console.log(key, vehicle[key]);
     }
-};
+}
+
+bike.needsRepair = true;
+skateboard.needsRepair = true;
+
+button.addEventListener("click", function() {
+    repairList.innerHTML = ""; // clear list before populating it
+
+    const vehicleRepairList = myVehicles.filter(function (vehicle) {
+        return vehicle.needsRepair === true;
+    });
+    
+    
+    for(let vehicle of vehicleRepairList) {
+        let li = document.createElement("li");
+        li.innerHTML = `My <span>${vehicle.type}</span> needs some love`;
+        repairList.append(li);
+    }
+    console.log(vehicleRepairList);
+});
